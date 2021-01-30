8acf91303d4ee11fcc89add3005f8147997a1d44
bublikk <yuliya.vlasovaa@gmail.com>
2021-01-31 00:06:29 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -2,22 +2,18 @@
 // диапазон только положительный (вкл 0)
 // если значение max <= min, то вернет -1
 
-const randomInteger = function (min, max) {
-  if (min >= 0 && max > min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  return -1;
+const getRandomIntInclusive = (min, max) => {
+  min = Math.floor(min);
+  max = Math.floor(max);
+  return min >= 0 && max > min ? Math.floor(Math.random() * (max - min + 1)) + min : -1;
 }
 
-randomInteger(1,400);
+getRandomIntInclusive(1.5,20);
 
 //проверка максимальной длины строки
 
-const сheckStringLength  = function (initialString, maxLength) {
-  if (initialString !== '' && initialString.length <= maxLength) {
-    return true;
-  }
-  return false;
+const checkStringLength = (initialString, maxLength) => {
+  return initialString.length <= maxLength;
 }
 
-сheckStringLength('Статическое свойство string.length возвращает значение.', 100);
+checkStringLength('', 10);
