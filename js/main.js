835a3b42208bbac80b8aaac99207260c39ba6bcb
bublikk <yuliya.vlasovaa@gmail.com>
2021-02-05 18:29:31 +0300

    bugfix code review

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//массив с текстом комментариев
+// массив с текстом комментариев
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -8,7 +8,7 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-//массив юзернеймов
+// массив юзернеймов
 const NAMES = [
   'Иван',
   'Хуан Себастьян',
@@ -20,8 +20,8 @@ const NAMES = [
   'Вашингтон',
 ];
 
-//пустой массив для проверки уникальности id комментария (заполняется из функции getUniqueId)
-const UNIQUE_IDS = [];
+// пустой массив для проверки уникальности id комментария (заполняется из функции getUniqueId)
+const unique_ids = [];
 
 // возвращает случайное целое число от min (вкл) до max (вкл)
 // диапазон только положительный (вкл 0)
@@ -34,24 +34,24 @@ const getRandomIntInclusive = function (min, max) {
     : -1;
 };
 
-//блок генерации и проверки уникального id комментария
+// блок генерации и проверки уникального id комментария
 const getUniqueId = function () {
-  let commentId = getRandomIntInclusive(1, 1000);
-  while (UNIQUE_IDS.indexOf(commentId) !== -1) { //пока переменная commentId не найдена в массиве UNIQUE_IDS цикл генерирует новое число
+  let commentId = getRandomIntInclusive(1, 1000); // 1000 тоже случайное число
+  while (unique_ids.indexOf(commentId) !== -1) { // пока переменная commentId не найдена в массиве unique_ids цикл генерирует новое число
     commentId = getRandomIntInclusive(1, 1000);
   }
-  UNIQUE_IDS.push(commentId);
+  unique_ids.push(commentId);
   return commentId;
 }
 
-//блок комментария (comments - массив объектов)
+// блок комментария (comments - массив объектов)
 const getCommentData = function () {
-  let commentData = [];
-  let commentTotal = getRandomIntInclusive(1, 15);
-  for (let j = 1; j <= commentTotal; j++){
-    let comment = {
+  const commentData = [];
+  let commentTotal = getRandomIntInclusive(1, 15); // кол-во на свое усмотрение
+  for (let j = 1; j <= commentTotal; j++) {
+    const comment = {
       id: getUniqueId(),
-      avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,  //количество аватаров
+      avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,  // путь до изображения
       message: MESSAGES[getRandomIntInclusive(0, MESSAGES.length - 1)],
       name: NAMES[getRandomIntInclusive(0, NAMES.length - 1)],
     }
@@ -62,9 +62,9 @@ const getCommentData = function () {
 
 //блок генерации массива объктов, где объекты это данные поста
 const getPhotoData = function () {
-  let photoData = [];
+  const photoData = [];
   for (let i = 1; i <= 25; i++) {
-    let post = {
+    const post = {
       id: i,
       url: `photos/${i}.jpg`,
       description: `Подпись к фото ${i}`,
