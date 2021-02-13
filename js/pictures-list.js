27a847ded00ba78f731e1e057b53cc4d725aaf37
bublikk <yuliya.vlasovaa@gmail.com>
2021-02-13 22:32:51 +0300

    Добавила комментарии для себя

--- js/pictures-list.js
+++ js/pictures-list.js
@@ -1,15 +1,15 @@
-import {getPhotoData} from './get-photo-data.js';
+import {getPhotoData} from './get-photo-data.js'; // Импорт функции getPhotoData
 
-const picturesList = getPhotoData();
+const picturesList = getPhotoData(); // Данная переменная хранит результат вызванной функции (массив)
 
 const pictures = document.querySelector('.pictures');
 const templateFragment = document.querySelector('#picture').content; // Находим фрагмент с содержимым темплейта
 const template = templateFragment.querySelector('a'); // В фрагменте находим нужный элемент
-const fragment = document.createDocumentFragment();
+const fragment = document.createDocumentFragment(); // Создаем коробочку для хранения сгенерированных элементов
 
-picturesList.forEach(function (pictureItem) {
-  const element = template.cloneNode(true);
-  element.querySelector('img').src = pictureItem.url;
+picturesList.forEach(function (pictureItem) { // Обходим массив pictureList (итератор pictureItem)
+  const element = template.cloneNode(true); // Клонируем шаблон (создаем элемент)
+  element.querySelector('.picture__img').src = pictureItem.url; // Находим в этом элементе img и прописываем в его свойство "src" значение равное значению из элемента нашего массива pictureList
   element.querySelector('.picture__likes').textContent = pictureItem.likes;
   element.querySelector('.picture__comments').textContent = pictureItem.comments.length;
   fragment.appendChild(element); // Добавляем элементы в наш фрагмент
